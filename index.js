47dda2686be2b954e7d0109a846e40766c9c6648
kansd1401 <kang.sanjeet1401@gmail.com>
2019-10-18 20:40:48 +0000

    made it recursive

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 
 function sum(fromN, toN) {
-  // Sum all the values from fromN up to toN
+  /*if (fromN === toN) {
+    return fromN;
+  }
+  return fromN + sumToOne(fromN - 1,toN);*/
+  if(fromN === toN){
+    return toN;
+  }
+  return fromN + sum(fromN + 1,toN)
 }
 
 module.exports = sum;
